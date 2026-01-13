f3e63da5516b558f23e8aaebe96f23650b2ba997
_niranjanv_ <niranjanv2k4@gmail.com>
2026-01-13 14:24:17 +0530

    Halloumi Boxes

---
+++ 800/1903A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    string s;
+    cin >> n >> s;
+    
+    int count = 0, res = 0;
+    bool flag = false;
+    for(int i = 0; i<n; i++){
+      if(s[i] == '.'){
+        count++;
+        if(count == 3){
+            flag = true;
+            break;
+        }
+      }else
+        count = 0;
+    }
+    if(flag)
+        res = 2;
+    else
+        for(int el:s)
+            res += (el == '.');
+        
+    cout << res << "\n";
+  }
+  return 0;
+}
