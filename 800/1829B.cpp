9e3039e98a9a53f37641cc9400f92c3a0dd1fc4b
_niranjanv_ <niranjanv2k4@gmail.com>
2026-01-31 11:31:55 +0530

    Blank space

---
+++ 800/1829B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i = 0; i<n; i++)
+            cin >> arr[i];
+    
+        int res = 0, curr = 0;
+        for(int i = 0; i<n; i++){
+            if(arr[i] == 0){
+                curr++;
+                res = max(res, curr);
+            }else
+                curr = 0;
+        }
+
+        cout << res << "\n";
+    }
+
+
+    return 0;
+}
