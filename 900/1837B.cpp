a8f149b3a3177c39220257039306e94a01bc55cc
_niranjanv_ <niranjanv2k4@gmail.com>
2026-05-19 10:22:50 +0530

    Comparison String

---
+++ 900/1837B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        string s;
+        cin >> n >> s;
+        
+        int curr = 1;
+        int max_len = 1;
+
+        for(int i = 1; i<n; i++){
+            if(s[i] == s[i-1]){
+                curr++;
+                max_len = max(max_len, curr);
+            }else{
+                curr = 1;
+            }
+        }
+
+        cout << max_len + 1<< "\n";
+        
+    }
+
+    return 0;
+}
