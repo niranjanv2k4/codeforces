acd7295258491111f9b3832248d358266c48f841
_niranjanv_ <niranjanv2k4@gmail.com>
2026-06-06 09:45:02 +0530

    Odd Divisor

---
+++ 900/1475A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        long long n;
+        cin >> n;
+
+        cout << ((n & (n-1)) == 0 ? "NO\n" : "YES\n");
+    }
+
+    return 0;
+}
